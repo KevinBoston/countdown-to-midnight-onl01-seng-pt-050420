3aab1466c483e7a9ac939a3c2c2d760f7b11c43a
Kevin Boston <kbos22@gmail.com>
2020-05-04 19:00:57 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -10,10 +10,11 @@ def countdown(x)
 end
 
 def countdown_with_sleep(x)
-  
-  
-  
-  
-  
-  
+   while x > 0
+  puts "#{x} SECOND(S)!"
+  sleep 60
+#  x -= 1
+  x == 0 ? "HAPPY NEW YEAR!" : x -= 1
+  end
+   "HAPPY NEW YEAR!"
 end
